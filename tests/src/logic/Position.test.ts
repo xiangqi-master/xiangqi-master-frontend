4488d9aba8f6dc5ed3041b175128303825d17d1e
Wu Zengfu <wuzengfu666@gmail.com>
2024-12-15 23:01:05 +0400

    test: add test cases of isCrossRiver and equals for Position.ts

--- tests/src/logic/Position.test.ts
+++ tests/src/logic/Position.test.ts
@@ -39,3 +39,54 @@ describe("Test: isCrossRiver", () => {
     expect(new Position(0, 100).isCrossRiver(false)).toBeFalsy()
   })
 })
+
+describe("Test: isCrossRiver", () => {
+  it("isRed === true && isCrossRiver === true", () => {
+    expect(new Position(0, 5).isCrossRiver(true)).toBeTruthy()
+    expect(new Position(0, 100).isCrossRiver(true)).toBeTruthy()
+    expect(new Position(100, 5).isCrossRiver(true)).toBeTruthy()
+  })
+
+  it("isRed === true && isCrossRiver === false", () => {
+    expect(new Position(0, 4).isCrossRiver(true)).toBeFalsy()
+    expect(new Position(-100, 4).isCrossRiver(true)).toBeFalsy()
+    expect(new Position(0, 0).isCrossRiver(true)).toBeFalsy()
+  })
+
+  it("isRed === false && isCrossRiver === true", () => {
+    expect(new Position(0, 4).isCrossRiver(false)).toBeTruthy()
+    expect(new Position(100, 4).isCrossRiver(false)).toBeTruthy()
+    expect(new Position(0, -1).isCrossRiver(false)).toBeTruthy()
+  })
+
+  it("isRed === false && isCrossRiver === false", () => {
+    expect(new Position(0, 5).isCrossRiver(false)).toBeFalsy()
+    expect(new Position(0, 100).isCrossRiver(false)).toBeFalsy()
+    expect(new Position(100, 5).isCrossRiver(false)).toBeFalsy()
+  })
+})
+
+describe("Test: equals", () => {
+  it("equals === true", () => {
+    let position1 = new Position(1, 3)
+    let position2 = new Position(1, 3)
+    expect(position1.equals(position2)).toBeTruthy()
+  })
+
+  it("equals === false due to unequal positions", () => {
+    let position1 = new Position(1, 3)
+    let position2 = new Position(2, 3)
+    expect(position1.equals(position2)).toBeFalsy()
+
+    position1 = new Position(1, 3)
+    position2 = new Position(1, 4)
+    expect(position1.equals(position2)).toBeFalsy()
+  })
+
+  it("equals === false due to null position", () => {
+    let position1 = new Position(1, 3)
+    let position2 = null
+    // @ts-ignore
+    expect(position1.equals(position2)).toBeFalsy()
+  })
+})
