bc7f971628c4408adae22e27dc9afbb4fc07b79a
Wu Zengfu <wuzengfu666@gmail.com>
2025-03-19 22:01:49 +0800

    update rook logic

--- src/logic/pieces/Rook.ts
+++ src/logic/pieces/Rook.ts
@@ -5,27 +5,32 @@ import { Range } from "immutable"
 import Optional from "../utils/Optional"
 
 class Rook extends Piece {
+  public constructor(code: number, position: Position) {
+    super(code, position)
+  }
+
   public override getAllValidMoves(board: Board): Position[] {
     const [x, y] = this.getPosition().getPosition()
 
     // [x-increment, y-increment, start, end]
     const moveDirections = [
-      [0, 1, y, 10],
+      [0, 1, y, 9],
       [0, -1, y, 0],
-      [1, 0, x, 9],
+      [1, 0, x, 8],
       [-1, 0, x, 0]
     ]
 
     return moveDirections
       .map((direction) =>
         Range(direction[2], direction[3], direction[0] + direction[1]).map(
-          (i) => new Position(x + i * direction[0], y + i * direction[1])
+          (_, i) =>
+            new Position(x + (i + 1) * direction[0], y + (i + 1) * direction[1])
         )
       ) // construct all possible positions
       .map((positions) =>
         positions.takeUntil((position) =>
           Optional.ofNullable(board.getPieceByPosition(position))
-            .filter((piece) => piece.isRed() !== super.isRed())
+            .filter((piece) => piece.isRed() === super.isRed())
             .isPresent()
         )
       ) // take all positions before the first piece with same color is met
@@ -33,11 +38,13 @@ class Rook extends Piece {
         positions
           .reverse()
           .skip(
-            positions.findLastIndex((position) =>
-              Optional.ofNullable(
-                board.getPieceByPosition(position)
-              ).isPresent()
-            ) + 1
+            positions
+              .reverse()
+              .findIndex((position) =>
+                Optional.ofNullable(
+                  board.getPieceByPosition(position)
+                ).isPresent()
+              )
           )
           .toArray()
       ) // skip all positions after the first opponent piece is met
