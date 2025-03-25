649da6325bc4f1e36a22a5a0d0285376a04c9c5d
Zhang Cheng <2540310276@qq.com>
2025-03-25 15:47:56 +0800

    feat: Added Elephant.ts

---
+++ src/logic/pieces/Elephant.ts
@@ -0,0 +1,54 @@
+import Piece from "../Piece"
+import Board from "../Board"
+import Position from "../Position"
+import Optional from "../utils/Optional"
+
+/**
+ * A class to represent an Elephant in Chinese Chess.
+ */
+class Elephant extends Piece {
+  public constructor(code: number, position: Position) {
+    super(code, position)
+  }
+
+  public override getAllValidMoves(board: Board): Position[] {
+    const isRed: boolean = super.isRed()
+    const riverBoundary = isRed ? 5 : 4 // Red (y <= 4), Black (y >= 5)
+    const moveDirections: [number, number][] = [
+      [2, 2],
+      [2, -2],
+      [-2, 2],
+      [-2, -2]
+    ]
+
+    return moveDirections
+      .map(
+        ([dx, dy]) =>
+          new Position(
+            this.getPosition().getX() + dx,
+            this.getPosition().getY() + dy
+          )
+      )
+      .filter((position) => position.isWithinBoundary())
+      .filter((position) =>
+        isRed
+          ? position.getY() <= riverBoundary
+          : position.getY() >= riverBoundary
+      ) // Cannot cross river
+      .filter((position) => {
+        const eyeX = (this.getPosition().getX() + position.getX()) / 2
+        const eyeY = (this.getPosition().getY() + position.getY()) / 2
+        return !board.getPieceByPosition(new Position(eyeX, eyeY)) // Check if elephant eye is blocked
+      })
+      .filter((position) => {
+        const piece = board.getPieceByPosition(position)
+        return !piece || piece.isRed() !== isRed // Can move if empty or occupied by opponent
+      })
+  }
+
+  public override toString(): string {
+    return "E"
+  }
+}
+
+export default Elephant
