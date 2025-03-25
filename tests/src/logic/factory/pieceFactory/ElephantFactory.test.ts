d3823c8d99d82fef80e1f7ff08c119e53be95bda
Zhang Cheng <2540310276@qq.com>
2025-03-25 15:54:46 +0800

    feat: Added ElephantFactory.test.ts

---
+++ tests/src/logic/factory/pieceFactory/ElephantFactory.test.ts
@@ -0,0 +1,23 @@
+import ElephantFactory from "../../../../../src/logic/factory/pieceFactory/ElephantFactory"
+import Position from "../../../../../src/logic/Position"
+import Elephant from "../../../../../src/logic/pieces/Elephant"
+
+describe("ElephantFactory", () => {
+  test("isRed === true", () => {
+    const position = new Position(6, 8)
+    const elephant = new ElephantFactory().createPiece(true, position)
+    expect(elephant).toBeInstanceOf(Elephant)
+    expect(elephant.getPosition().equals(position)).toBeTruthy()
+    expect(elephant.getCode()).toBe(4)
+    expect(elephant.isRed()).toBeTruthy()
+  })
+
+  test("isRed === false", () => {
+    const position = new Position(1, 2)
+    const elephant = new ElephantFactory().createPiece(false, position)
+    expect(elephant).toBeInstanceOf(Elephant)
+    expect(elephant.getPosition().equals(position)).toBeTruthy()
+    expect(elephant.getCode()).toBe(14)
+    expect(elephant.isRed()).toBeFalsy()
+  })
+})
